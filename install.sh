2d960a45ce6f3164e04cb90c82ab0aff2805ada0
Mbah Shondong <l4nangproduction@gmail.com>
2022-09-11 21:23:42 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+echo "Download ngrok"
+wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngok.sh && chmod +x ngrok.sh && ./ngrok.sh
+echo Downloading File
+echo "===================================="
+echo "ENTENI SAK WETORO"
+echo "UDUD NGOPI SEK MAS"
+echo "RAONO 2 MENIT KOK CUY"
+echo "===================================="
+sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install BROWSER CHROME"
+echo "===================================="
+sudo apt install chrome -y > /dev/null 2>&1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "===================================="
+echo "START RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "WES JOSS MAS"
+echo "IKI OJO DI TUTUP LHO YA"
+echo "NEK DITUTUP PODOWAE SHUT DOWN"
+echo "===================================="
+sleep 99999999999
