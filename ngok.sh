49320374dccb205f00f8bf6f5f08d10795f5b017
Mbah Shondong <l4nangproduction@gmail.com>
2022-09-11 21:18:04 +0700

    Create ngok.sh

---
+++ ngok.sh
@@ -0,0 +1,21 @@
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/intalngok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
+echo "======================="
