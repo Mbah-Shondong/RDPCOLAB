c98af43a9e05494476305c8d8f8b0f71ba5c1f9d
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-11 20:21:57 +0700

    Update windos.sh

--- windos.sh
+++ windos.sh
@@ -1,31 +1,16 @@
-echo "===================================="
-echo "Download windows files"
-echo "===================================="
-curl -L -o w7x86.img https://bit.ly/windosmbah
-echo "===================================="
-echo "Download ngrok"
-echo "===================================="
-wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh 
-nohup ./ngrok tcp 3388 &>/dev/null &
-./ngrok tcp 3388 &>/dev/null &
-echo "===================================="
-echo Downloading File 
-echo "===================================="
-apt-get install qemu > /dev/null 2>&1
-echo "===================================="
-echo "Wait"
-echo "Starting Windows"
-echo "===================================="
-echo RDP Address:
-echo "===================================="
+wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+sudo apt-get update > /dev/null 2>&1
+sudo apt install ubuntu-desktop-minimal > /dev/null 2>&1
+echo "Menginstal Firefox"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "Menginstal XRDP"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo adduser xrdp ssl-cert > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Ctrl+C To Copy"
-echo "Wait 1-2 minute to finish bot"
-echo "Dont Close This Tab"
-echo "awokawok"
-echo "===================================="
-echo "===================================="
-qemu-system-x86_64 -hda w7x86.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic > /dev/null 2>&1
+echo "----------------------------------------------------------------------"
+echo "- WES JOSS MAS IKI OJO DI TUTUP LHO YA NEK DITUTUP PODOWAE SHUT DOWN -"
+echo "----------------------------------------------------------------------"
 sleep 43200
