e68f453ef1bcffe0ec21b455a5cca8a12a671ab6
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-12 18:24:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 sudo apt-get update > /dev/null 2>&1
-sudo apt-get install lxde > /dev/null 2>&1
+sudo apt install xfce4 xorg dbus-x11 x11-xserver-utils > /dev/null 2>&1
 echo "Menginstal Firefox"
 sudo apt install firefox -y > /dev/null 2>&1
+echo "Install Brave"
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
 echo "Menginstal XRDP"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo adduser xrdp ssl-cert > /dev/null 2>&1
