485bc5f0e44d165508256903fe53dfd7416449ef
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-13 23:44:57 +0700

    Update shellcloud.sh

--- shellcloud.sh
+++ shellcloud.sh
@@ -14,7 +14,7 @@ echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3389 &>/dev/null &
 clear
-sudo apt-get install lxde -y
+docker pull frxyt/xrdp:xfce
 clear
 sudo apt-get install firefox -y > /dev/null 2>&1
 sudo apt install apt-transport-https curl > /dev/null 2>&1
@@ -23,12 +23,12 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=
 sudo apt update > /dev/null 2>&1
 sudo apt install brave-browser > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 clear
 echo "=======================100%"
-echo "Username : root"
-echo "Username : that you made yourself"
+echo "Username : debian"
+echo "Password : ChangeMe"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
