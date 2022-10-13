c203a199b7e5a87d3ea7271205c55690ebf20ea5
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-13 23:12:56 +0700

    Update shellcloud.sh

--- shellcloud.sh
+++ shellcloud.sh
@@ -1,6 +1,5 @@
-echo -e "$123\n$123\n" | sudo passwd
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -13,13 +12,28 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3388 &>/dev/null &
-docker pull frxyt/xrdp:cinnamon
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 clear
-echo "Start RDP"
-echo "Username : debian"
-echo "Password : ChangeMe"
+sudo apt-get install lxde -y
+clear
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install firefox -y > /dev/null 2>&1
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+clear
+echo "=======================100%"
+echo "Username : root"
+echo "Username : that you made yourself"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-docker run --rm -p 3388:3389 frxyt/xrdp:cinnamon > /dev/null 2>&1
-sleep 999999
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+sleep 9876543210
