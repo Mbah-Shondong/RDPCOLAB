5e877f792cdedb67bc159cc93cd89f552ecbb2b9
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-15 08:06:23 +0700

    Update shellcloud.sh

--- shellcloud.sh
+++ shellcloud.sh
@@ -10,11 +10,10 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3388 &>/dev/null &
-docker pull frxyt/xrdp:xfce
-echo "Username : debian"
-echo "Password : ChangeMe"
+docker pull danielguerra/ubuntu-xrdp:kali
+echo "Username : user pasword ubuntu"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "ojo di tutup"
-docker run --rm -p 3388:3389 frxyt/xrdp:xfce > /dev/null 2>&1
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
 sleep 999999
