f7416fa25bf9bb49173bf85988f9760333e45bfb
Mbah Shondong <l4nangproduction@gmail.com>
2022-10-22 21:30:28 +0700

    Update windos.sh

--- windos.sh
+++ windos.sh
@@ -1,16 +1,13 @@
+echo -e "$123\n$123\n" | sudo passwd
 wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 sudo apt-get update > /dev/null 2>&1
-sudo apt install xfce4 xorg dbus-x11 x11-xserver-utils > /dev/null 2>&1
-echo "Menginstal Firefox"
-sudo apt install firefox -y > /dev/null 2>&1
-echo "Menginstal XRDP"
-sudo apt install -y xrdp > /dev/null 2>&1
-sudo adduser xrdp ssl-cert > /dev/null 2>&1
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
-echo XRDP Address:
+docker pull danielguerra/alpine-xfce4-xrdp
+echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "----------------------------------------------------------------------"
-echo "- WES JOSS MAS IKI OJO DI TUTUP LHO YA NEK DITUTUP PODOWAE SHUT DOWN -"
-echo "----------------------------------------------------------------------"
-sleep 43200
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 danielguerra/alpine-xfce4-xrdp > /dev/null 2>&1
