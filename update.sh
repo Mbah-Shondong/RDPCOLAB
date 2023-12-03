00858033d6403c78b5b5102923767b676f619f66
Mbah Shondong <l4nangproduction@gmail.com>
2023-12-03 19:59:31 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,22 @@
+echo "Download dan Install NGROK "
+wget https://raw.githubusercontent.com/Mbah-Shondong/RDPCOLAB/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+echo "Install Desktop Environment"
+sudo apt install xfce4 xfce4-terminal xorg dbus-x11 x11-xserver-utils -y
+clear
+echo "Update Repository"
+sudo apt update > /dev/null 2>&1
+echo "Install Firefox"
+sudo apt install firefox -y > /dev/null 2>&1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+echo "Install XRDP"
+sudo apt install xrdp -y > /dev/null 2>&1
+sudo adduser xrdp ssl-cert > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "XRDP RUNNING ON"
+sudo service xrdp restart > /dev/null 2>&1
+echo "XRDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "----------------------------------------------------------------------"
+echo "----------------------------------------------------------------------"
+sleep 43200
